33e811ad8679ae0f1eca8241f34421898e5cfece
Tom Phillips <tphillips@greenhouse.io>
2016-03-19 00:17:09 -0400

    config stuff.

---
+++ tests/Bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', 1);
+
+$root       = realpath(dirname(dirname(__FILE__)));
+$library    = "$root/src";
+$tests      = "$root/tests";
+$path       = array($library, $tests, get_include_path());
+
+set_include_path(implode(PATH_SEPARATOR, $path));
+$vendorFilename = dirname(__FILE__) . '/../vendor/autoload.php';
+
+require $vendorFilename;
